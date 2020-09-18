a551bbeebcf4a7f81b580aac3e6b83c3c533fd07
Bruno Leite <brunoleitt@gmail.com>
2020-09-18 00:26:48 -0300

    Incluindo modulo de discovery service e gateway

--- cotacao/src/main/java/com/seguros/residencial/cotacao/CotacaoApplication.java
+++ cotacao/src/main/java/com/seguros/residencial/cotacao/CotacaoApplication.java
@@ -3,12 +3,14 @@ package com.seguros.residencial.cotacao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
 @EntityScan({"com.seguro.residencial.core.model"})
+@EnableEurekaClient
 @EnableJpaRepositories({"com.seguro.residencial.core.repository"})
+@SpringBootApplication
 public class CotacaoApplication {
     public static void main(String[] args) {
         SpringApplication.run(CotacaoApplication.class, args);
--- cotacao/src/test/java/com/seguros/residencial/cotacao/CotacaoApplicationTests.java
+++ cotacao/src/test/java/com/seguros/residencial/cotacao/CotacaoApplicationTests.java
@@ -1,13 +1,9 @@
 package com.seguros.residencial.cotacao;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CotacaoApplicationTests {
-
-    @Test
     void contextLoads() {
     }
-
 }
