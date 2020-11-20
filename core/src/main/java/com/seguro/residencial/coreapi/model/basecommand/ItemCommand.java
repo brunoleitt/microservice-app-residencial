56077a48a851ecd320f5824aebd4caf172d355b4
Bruno Leite <brunoleitt@gmail.com>
2020-11-20 17:39:06 -0300

    - Realizar a consulta de todos os serviços disponivel para o Tipo de Risco e Pacote de cobertura.

---
+++ core/src/main/java/com/seguro/residencial/coreapi/model/basecommand/ItemCommand.java
@@ -0,0 +1,21 @@
+package com.seguro.residencial.coreapi.model.basecommand;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+/**
+ * @criado 07/11/2020 - 15:48
+ * @projeto Seguro Residencial Simplificado
+ * @autor Bruno Leite
+ */
+
+@Getter
+@AllArgsConstructor
+public class ItemCommand {
+
+    @TargetAggregateIdentifier
+    public Long idItem;
+
+}
