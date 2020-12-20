36f3b30122f6c398bbb9e84bda75867635167fbb
Bruno Leite <brunoleitt@gmail.com>
2020-12-20 10:54:31 -0300

    ajuste no relacionamento de item e endereco item

--- residencial/src/main/resources/db/migration/V001__CRIACAO-DB-RESIDENCIAL_DDL.sql
+++ residencial/src/main/resources/db/migration/V001__CRIACAO-DB-RESIDENCIAL_DDL.sql
@@ -1,28 +1,12 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema seguros
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `seguros` ;
-
--- -----------------------------------------------------
--- Schema seguros
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `seguros` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `seguros` DEFAULT CHARACTER SET utf8mb4 ;
 USE `seguros` ;
 
 -- -----------------------------------------------------
 -- Table `seguros`.`application_user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`application_user` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`application_user` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `password` VARCHAR(255) NOT NULL,
@@ -35,15 +19,40 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `seguros`.`tipo_risco`
+-- Table `seguros`.`status_cotacao`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`tipo_risco` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`tipo_risco` (
-  `id` BIGINT NOT NULL,
-  `descricao` VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `seguros`.`status_cotacao` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `seguros`.`tipo_calculo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`tipo_calculo` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `seguros`.`tipo_vigencia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`tipo_vigencia` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -51,9 +60,31 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `seguros`.`profissao`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`profissao` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`profissao` (
+  `id` BIGINT NOT NULL,
+  `descricao` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `seguros`.`sexo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`sexo` (
+  `id` BIGINT NOT NULL,
+  `descricao` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `seguros`.`tipo_risco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`tipo_risco` (
   `id` BIGINT NOT NULL,
   `descricao` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
@@ -65,8 +96,6 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `seguros`.`cobertura_pacote`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`cobertura_pacote` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`cobertura_pacote` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(30) NOT NULL,
@@ -86,8 +115,6 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `seguros`.`cobertura`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`cobertura` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`cobertura` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(100) NOT NULL,
@@ -107,8 +134,6 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `seguros`.`cobertura_servicos`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`cobertura_servicos` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`cobertura_servicos` (
   `id_coberturas_servicos` BIGINT NOT NULL,
   `descricao` VARCHAR(30) NOT NULL,
@@ -125,55 +150,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `seguros`.`status_cotacao`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`status_cotacao` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`status_cotacao` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `seguros`.`tipo_calculo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`tipo_calculo` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`tipo_calculo` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `seguros`.`tipo_vigencia`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`tipo_vigencia` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`tipo_vigencia` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `seguros`.`cotacao`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`cotacao` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`cotacao` (
   `codigo_cotacao` VARCHAR(255) NOT NULL,
   `data_atualizacao` DATETIME(6) NULL DEFAULT NULL,
@@ -199,76 +177,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `seguros`.`item_endereco`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`item_endereco` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`item_endereco` (
-  `id` VARCHAR(50) NOT NULL,
-  `cep` VARCHAR(8) NOT NULL,
-  `cidade` VARCHAR(20) NOT NULL,
-  `complemento` VARCHAR(30) NOT NULL,
-  `logradouro` VARCHAR(30) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `uf` VARCHAR(2) NOT NULL,
-  `id_item` VARCHAR(50) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FK_item_itemEndereco`
-    FOREIGN KEY (`id_item`)
-    REFERENCES `seguros`.`item` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `seguros`.`item`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`item` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`item` (
-  `id` VARCHAR(50) NOT NULL,
-  `id_pacote_cobertura` BIGINT NULL DEFAULT NULL,
-  `id_cotacao` VARCHAR(255) NULL DEFAULT NULL,
-  `id_tipo_risco` BIGINT NULL DEFAULT NULL,
-  `endereco_item_id` VARCHAR(50) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_cotacaoItem`
-    FOREIGN KEY (`id_cotacao`)
-    REFERENCES `seguros`.`cotacao` (`codigo_cotacao`),
-  CONSTRAINT `fk_pacoteCobertura`
-    FOREIGN KEY (`id_pacote_cobertura`)
-    REFERENCES `seguros`.`cobertura_pacote` (`id`),
-  CONSTRAINT `fk_tipoRisco`
-    FOREIGN KEY (`id_tipo_risco`)
-    REFERENCES `seguros`.`tipo_risco` (`id`),
-  CONSTRAINT `FKe8jowhdlfoo71wu0fbtel2uiv`
-    FOREIGN KEY (`endereco_item_id`)
-    REFERENCES `seguros`.`item_endereco` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `seguros`.`profissao`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`profissao` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`profissao` (
-  `id` BIGINT NOT NULL,
-  `descricao` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `seguros`.`questionario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`questionario` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`questionario` (
   `id` VARCHAR(50) NOT NULL,
   `possui_anti_furto` BIT(1) NOT NULL,
@@ -283,24 +193,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `seguros`.`sexo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`sexo` ;
-
-CREATE TABLE IF NOT EXISTS `seguros`.`sexo` (
-  `id` BIGINT NOT NULL,
-  `descricao` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `seguros`.`segurado`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `seguros`.`segurado` ;
-
 CREATE TABLE IF NOT EXISTS `seguros`.`segurado` (
   `id` VARCHAR(50) NOT NULL,
   `cpf` VARCHAR(11) NOT NULL,
@@ -327,6 +221,44 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- -----------------------------------------------------
+-- Table `seguros`.`item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`item` (
+  `id` VARCHAR(50) NOT NULL,
+  `id_pacote_cobertura` BIGINT NULL DEFAULT NULL,
+  `id_cotacao` VARCHAR(255) NULL DEFAULT NULL,
+  `id_tipo_risco` BIGINT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_cotacaoItem`
+    FOREIGN KEY (`id_cotacao`)
+    REFERENCES `seguros`.`cotacao` (`codigo_cotacao`),
+  CONSTRAINT `fk_pacoteCobertura`
+    FOREIGN KEY (`id_pacote_cobertura`)
+    REFERENCES `seguros`.`cobertura_pacote` (`id`),
+  CONSTRAINT `fk_tipoRisco`
+    FOREIGN KEY (`id_tipo_risco`)
+    REFERENCES `seguros`.`tipo_risco` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `seguros`.`item_endereco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `seguros`.`item_endereco` (
+  `id` VARCHAR(50) NOT NULL,
+  `cep` VARCHAR(8) NOT NULL,
+  `cidade` VARCHAR(20) NOT NULL,
+  `complemento` VARCHAR(30) NOT NULL,
+  `logradouro` VARCHAR(30) NOT NULL,
+  `numero` VARCHAR(10) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `id_item` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_item_itemEndereco`
+    FOREIGN KEY (`id_item`)
+    REFERENCES `seguros`.`item` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
